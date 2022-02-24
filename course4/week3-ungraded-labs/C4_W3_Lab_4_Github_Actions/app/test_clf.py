bf278a8ab739d1e5623034a2c88440f0d446b6a6
phbillet <philippe.billet@seznam.cz>
2022-02-24 10:28:31 +0100

    Adding new classifier with scaling2

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
@@ -5,15 +5,17 @@ from sklearn.preprocessing import StandardScaler
 
 def test_pipeline_and_scaler():
 
-    # Check if clf is an instance of sklearn.pipeline.Pipeline 
+    # Check if clf is an instance of sklearn.pipeline.Pipeline
     isPipeline = isinstance(clf, Pipeline)
     assert isPipeline
-    
+
     if isPipeline:
-        # Check if first step of pipeline is an instance of 
+        # Check if first step of pipeline is an instance of
         # sklearn.preprocessing.StandardScaler
         firstStep = [v for v in clf.named_steps.values()][0]
-        assert isinstance(firstStep, StandardScaler)def test_accuracy():
+        assert isinstance(firstStep, StandardScaler)
+
+def test_accuracy():
 
     # Load test data
     with open("data/test_data.pkl", "rb") as file:
